7273191c51d31d34a28b90582c21d8081352d5ab
Raman Preet Singh <ramanpreetsingh8800@gmail.com>
2025-09-30 12:42:32 +1300

    test

--- 01_data_cleaning.R
+++ 01_data_cleaning.R
@@ -5,12 +5,4 @@ library(tidyverse)
 # loading data
 df <- mtcars
 
-print(df)
-
-# filter high performance cars
-high_performance_cars <- df %>% 
-  filter(hp > 200)
-
-# filter cars with more than 4 cylinders and horsepower less than 150
-less_efficient_cars <- df %>% 
-  filter(cyl > 4 & hp < 150)
+print(df) # to print the table
